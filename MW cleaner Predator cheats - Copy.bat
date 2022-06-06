696b9a03ee8e6dbdd5a9544bf6d48637f7c522ff
segation-dev <75533533+segation-dev@users.noreply.github.com>
2022-06-06 12:24:02 +0100

    Add files via upload

---
+++ MW cleaner Predator cheats - Copy.bat
@@ -0,0 +1,70 @@
+---Cleaner maybe Work ?
+
+
+
+
+@Echo off
+title Free cleaner and shit.
+cls
+color 0a
+
+echo Welcome to the Call of Duty Warzone Cleaner ITS IN BETA
+echo.
+echo !!! DISCLAIMER !!!
+echo PLEASE HAVE WARZONE AND BATTLE SOFTWARE / BLIZARD CLOSED SO IT CAN WORK!
+echo.
+echo --------------------------------------------------------------------------------------------
+echo - Warning 																					- 
+echo -YOU MUST RUN THIS .BAT TOOL AS WINDOWS ADMINISTRATOR!										-
+echo -																							-
+echo - You are about to delete game files and folders that are used by Activision and Blizzard 	-
+echo - Once completed, this system will be reset as if the game was freshly installed. 			-
+echo --------------------------------------------------------------------------------------------
+timeout /t 10 /nobreak > nul
+echo.
+echo Close this window to cancel...
+timeout /t 5 /nobreak > nul
+
+echo Finding MW Files ...
+timeout /t 2 /nobreak > nul
+echo ...
+timeout /t 2 /nobreak > nul
+echo ..
+timeout /t 2 /nobreak > nul
+echo .
+timeout /t 2 /nobreak > nul
+echo Found Files and folders
+timeout /t 2 /nobreak > nul
+echo.
+echo Cleaning trace files and folders...
+echo.
+Ping localhost -n 2 >Nul
+
+timeout /t 3 /nobreak > nul
+echo Task 1 of 2 - Removing files
+echo Removing tracking and trace files from the Call of Duty Modern Warfare main folder.
+echo.
+timeout /t 3 /nobreak > nul
+
+echo. Task 1 has been Completed
+timeout /t 2 /nobreak > nul
+echo.
+echo Task 2 of 2
+
+echo Removing Warzone and Blizzard Local Application and Program Data Folders
+timeout /t 3 /nobreak > nul
+echo Keeping Game Files.
+echo.
+
+echo.
+echo.
+echo ********************************************************************************************
+echo * System Cleanup Complete *
+echo * *
+echo * The game may reload shaders, because these files were cached and have been removed. *
+echo * In rare cases, the game might require a repair before it will launch. * 
+echo * *
+echo ********************************************************************************************
+echo.
+
+pause
